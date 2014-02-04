4326e93ec6c5fc77fa6679c806c0c9d0c023310b
J0eCool <count.j0ecool@gmail.com>
2014-02-04 20:07:52 +0000

    Clojure - Problem 1

---
+++ Clojure/prob1.clj
@@ -0,0 +1,8 @@
+(defn multipleOf [x n] (= (mod x n) 0))
+
+(defn threeOrFive [x] (or (multipleOf x 3) (multipleOf x 5)))
+
+(defn threeFiveSum [n]
+  (reduce + (filter threeOrFive (range n))))
+
+(threeFiveSum 1000)
