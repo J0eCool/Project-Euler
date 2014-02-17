47fb4ebaab5b4768bc763b2efaf9ce5e1a22d186
J0eCool <count.j0ecool@gmail.com>
2014-02-17 18:39:38 -0500

    Start of Clojure26

--- Clojure/prob26.clj
+++ Clojure/prob26.clj
@@ -7,6 +7,38 @@
    float))
 
 (invDecDigits 3)
+(invDecDigits 5)
+(invDecDigits 7)
 
-(map invDecDigits
-     (range 2 10))
+(defn repeatString [n]
+  (let [leadSubstr (fn [sub lst]
+                     (cond (= 0 (min (count sub) (count lst))) true
+                           (not= (first sub) (first lst)) false
+                           :else (recur (rest sub) (rest lst))))
+        doesRepeat (fn [rep lst]
+                     (if (= 0 (count lst)) true
+                       (if (leadSubstr rep lst)
+                         (recur rep (drop (count rep) lst))
+                         false)))
+        digits (invDecDigits n)]
+    (loop [rep [(first digits)]
+           lst digits]
+      (if (= 0 (count lst))
+        []
+        (if (>= (count rep) (count lst))
+          (let [new (rest lst)] (recur [(first new)] new))
+          (if (doesRepeat rep lst)
+            rep
+            (recur (concat rep [(nth lst (count rep))]) lst)))))))
+
+(repeatString 4)
+
+(let [pairs (map (fn [n]
+                   {:num n :len (count (repeatString n))})
+                 (range 2 1000))
+      ;pairs* (filter )
+      m (reduce #(if (> (:len %1) (:len %2)) %1 %2)
+                pairs)]
+  m)
+
+(invDecDigits 985)
